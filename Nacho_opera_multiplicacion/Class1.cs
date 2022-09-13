3ede21869be6be47f217c08746ef3e0d8cc6d3e3
Usuario <Usuario@B13-205-22868>
2022-09-13 11:32:20 -0500

    Agregar archivos de proyecto.

---
+++ Nacho_opera_multiplicacion/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nacho_opera_multiplicacion
+{
+    public class multiplicacion
+    {
+       /// <summary>
+       /// Metodo multiplicar
+       /// </summary>
+       /// <param name="i"> operando 1 </param>
+       /// <param name="j"> operando 2 </param>
+       /// <returns></returns>
+       ///
+       public static long multi(long i, long j)
+        {
+            return (i * j);
+        }
+    }
+}
